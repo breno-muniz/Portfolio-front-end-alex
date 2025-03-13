95b965a5731d9a4a919f6364323e6cb70aae3447
breno-muniz <brenomunizsilva@gmail.com>
2025-03-13 14:02:30 -0300

    Sessão Header e Menu Mobile
    Foi criada a sessão Header e o menu mobile com suas caracteristicas e tambem funcionalidades via Js

---
+++ Javascript/script.js
@@ -0,0 +1,28 @@
+// Menu Mobile
+
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', (e) => {
+            e.stopPropagation();
+            nav.classList.toggle('active-menu-mobile');
+            toggle.classList.toggle('active-bx');
+        });
+
+        nav.addEventListener('click', () => {
+            nav.classList.remove('active-menu-mobile');
+            toggle.classList.remove('active-bx');
+        });
+
+        document.body.addEventListener('click', () => {
+            if (nav.classList.contains('active-menu-mobile')) {
+                nav.classList.remove('active-menu-mobile');
+                toggle.classList.remove('active-bx');
+            }
+        });
+    }
+}
+
+showMenu('bx', 'menu-mobile');
