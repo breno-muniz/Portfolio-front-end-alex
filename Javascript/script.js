9f2194cd68488689b696130a6ae33aaf2555bc52
breno-muniz <brenomunizsilva@gmail.com>
2025-03-19 13:57:35 -0300

    Atualização dos icones e criação do modal
    Foi criado a sessão modal e tambem a atualização dos icones

--- Javascript/script.js
+++ Javascript/script.js
@@ -29,3 +29,57 @@ showMenu('bx', 'menu-mobile');
 
 
 // MODAL
+const button = document.querySelector('.button-modal');
+const modal = document.getElementById('mymodal');
+const close = document.querySelector('.close');
+
+button.addEventListener('click', () => {
+    modal.classList.add('modal-active');
+});
+
+close.addEventListener('click', () => {
+    modal.classList.remove('modal-active');
+});
+
+const projetos = [{
+    titulo: 'U.bank',
+    descricao: 'Aplicativo de banco digital',
+    img: 'Imagens/projetos/U.bank.png',
+    tituloTecnologias: 'Tecnologias utilizadas:',
+    tituloButtonGithub:"Ver no Github",
+    tituloButtonProjeto:"Ver Projeto",
+    github:"",
+    linkProjeto:"https://breno-muniz.github.io/Projeto-U.bank/",
+    tecnologies: ['Imagens/Icones/Figma.png', 'Imagens/Icones/Html.png', 'Imagens/Icones/Js.png', 'Imagens/Icones/sass.png', 'Imagens/Icones/css.png']
+}]
+
+const modalTitle = document.getElementById('modal-title');
+const imgModal = document.getElementById('img-modal');
+const modalDesc = document.querySelector('.descripition-p');
+const modalTecnologies = document.getElementById('tecnologias-modal');
+const modalTitleTecnologias = document.getElementById('tenclogias-title-modal');
+const buttonGitguhb= document.getElementById('github-button');
+const buttonProjeto= document.getElementById('button-projeto');
+
+
+
+buttonGitguhb.classList.add('github-button');
+buttonProjeto.classList.add('button-projeto');
+modalTitleTecnologias.textContent = projetos[0].tituloTecnologias;
+modalTitle.textContent = projetos[0].titulo;
+imgModal.src = projetos[0].img;
+modalDesc.textContent = projetos[0].descricao;
+buttonProjeto.textContent=projetos[0].tituloButtonProjeto;
+buttonGitguhb.href=projetos[0].github;
+buttonProjeto.href=projetos[0].linkProjeto;
+buttonProjeto.target="_blank";
+
+
+const tecnologies = projetos[0].tecnologies;
+
+tecnologies.forEach((tecnologies) => {
+    const img = document.createElement('img');
+    img.src = tecnologies;
+    modalTecnologies.appendChild(img);
+});
+
